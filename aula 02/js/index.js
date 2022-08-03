d94ffe6b270bb4f27d6345716f3017b189d9e882
Ricardo <rosastaricardo@gmail.com>
2022-08-03 20:40:13 -0300

    Exercício 2 finalizado

--- aula 02/js/index.js
+++ aula 02/js/index.js
@@ -1,6 +1,19 @@
+// Autores:
+
+// Diego Amorim
+
+// Filipe Rodrigues
+
+// Leonardo Araújo
+
+// João Guimarães
+
+// Jonathan Santos
+
+// Ricardo Silva
+
 function mostraMenu() {
-  console.log(`Escolha: Pedra, Papel ou Tesoura.`);
-  console.log(`================================= \n`);
+  alert(`Escolha: Pedra, Papel ou Tesoura.`);
 }
 
 function processaOpcao(jogador) {
@@ -20,15 +33,19 @@ function processaOpcao(jogador) {
   }
 }
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 let empate = 0;
 let numeroVitoriasA = 0;
 let numeroVitoriasB = 0;
 let vencedor = "";
 
 do {
-  const jogadorA = processaOpcao("jogadorA:");
+  const jogadorA = processaOpcao("Jogador:");
   console.log(`---------------------- \n`);
-  const jogadorB = processaOpcao("jogadorB:");
+  const jogadorB = getRandomInt(0, 3);
   console.log(`---------------------- \n`);
 
   if (jogadorA === jogadorB) empate++;
@@ -38,6 +55,9 @@ do {
 
   if (numeroVitoriasA === 2 && numeroVitoriasB < 2) vencedor = "A";
   if (numeroVitoriasB === 2 && numeroVitoriasA < 2) vencedor = "B";
+
+  if (numeroVitoriasA === 2 || numeroVitoriasB === 2) break;
+  alert(`O resultado parcial é: ${numeroVitoriasA} X ${numeroVitoriasB}`);
 } while (vencedor === "");
 
 alert(`O vencedor é: ${vencedor}`);
