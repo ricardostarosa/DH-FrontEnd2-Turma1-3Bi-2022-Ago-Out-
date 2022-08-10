3bbd2cb766acf07a3eaa8f83268c1eaf9702d504
ricardo <rosastaricardo84@gmail.com>
2022-08-10 20:50:48 -0300

    exercicio-darkmode-aula05

--- aula 05/lightDarkMode.js
+++ aula 05/lightDarkMode.js
@@ -1,24 +1,4 @@
-const cor = "dark";
-
-// function changeColorMode(mode) {
-//   document.querySelector("body").classList.toggle(mode);
-
-//   document.querySelector("h1").classList.toggle(mode);
-
-//   const itens = document.querySelectorAll(".item");
-
-//   itens.forEach((elements) => {
-//     elements.classList.toggle(mode);
-
-//     elements.childNodes[3].classList.toggle(mode);
-
-//     elements.childNodes[5].classList.toggle(mode);
-//   });
-// }
-
-// changeColorMode(cor);
-
-function myFunction() {
+function changeColorMode() {
   document.querySelector("body").classList.toggle("dark");
 
   document.querySelector("h1").classList.toggle("dark");
